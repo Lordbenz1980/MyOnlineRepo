0eef0416ab934763a7e1ce5597b79a557e6a3001
Lordbenz <lordbenz1980@gmail.ocm>
2015-11-09 14:04:25 +0100

    Calculator with mvc

---
+++ Java FX/src/com/baines/calculator/Model.java
@@ -0,0 +1,26 @@
+package com.baines.calculator;
+
+public class Model {
+	
+	public long calculate(long number1,long number2,String operator){
+		
+		switch (operator){
+		case "+":
+			return number1+number2;
+		case "-":
+			return number1-number2;
+		case "*":
+			return number1*number2;
+		case "/":
+			if(number2==0)
+			 return 0;
+			
+			return number1/number2;
+		
+		}
+		System.out.println("Unknown Operator - "+ operator);
+		return 0;
+		
+	}
+
+}
